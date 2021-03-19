0195a7199c972e121df743db7a5f537f8c12e90d
Amandeep Singh <programmeramandeep@gmail.com>
2021-03-19 21:25:22 +0530

    setup react redux

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,21 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
+// store
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-    // <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    // </React.StrictMode>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
