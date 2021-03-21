75464b3a6e6c2716c25164e9e4ca9bc9d466eadd
Amandeep Singh <programmeramandeep@gmail.com>
2021-03-21 13:27:35 +0530

    added logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
 
 import Header from "./components/nav/Header";
 import Home from "./pages/Home";
@@ -10,6 +12,28 @@ import Register from "./pages/auth/Register";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    // to check firebase auth store
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const idTokenResult = await user.getIdTokenResult();
+
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+            }
+        });
+
+        // cleanup
+        return () => unsubscribe();
+    }, []);
+
     return (
         <>
             <Header />
--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
 import {
     AppstoreOutlined,
+    LogoutOutlined,
     SettingOutlined,
     UserAddOutlined,
     UserOutlined,
 } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 
 const { Item, SubMenu } = Menu;
 
 const Header = () => {
     const [current, setCurrent] = useState("home");
+    let history = useHistory();
+    let disptach = useDispatch();
 
     const handleClick = (e) => {
         setCurrent(e.key);
+
+        disptach({
+            type: "LOGOUT",
+            payload: null,
+        });
+
+        history.push("/login");
+    };
+
+    const logout = () => {
+        firebase.auth().signOut();
     };
 
     return (
@@ -35,9 +52,17 @@ const Header = () => {
                 <Link to="/login">Login</Link>
             </Item>
 
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+            <SubMenu
+                key="SubMenu"
+                icon={<SettingOutlined />}
+                title="Username"
+                className="float-right"
+            >
                 <Item key="setting:1">Option 1</Item>
                 <Item key="setting:2">Option 2</Item>
+                <Item icon={<LogoutOutlined />} onClick={logout}>
+                    Logout
+                </Item>
             </SubMenu>
         </Menu>
     );
