949d0dc4166f259267e400dbf86da804eb5e3860
Amandeep Singh <amandeep.singh@cssindiaonline.com>
2021-03-23 10:05:05 +0530

    setup routes

--- server/server.js
+++ server/server.js
@@ -5,31 +5,30 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
+// import routes
+const authRoutes = require("./routes/auth");
+
 // app
 const app = express();
 
 // db
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB Connected"))
-    .catch((error) => console.log("DB Connection error", error));
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((error) => console.log("DB Connection error", error));
 
 // middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-// routes
-app.get("/api", (req, res) => {
-    res.json({
-        data: "hey you hit node api",
-    });
-});
+// middleware routes
+app.use("/api", authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
