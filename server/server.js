b227505872013eeb4b4ec2705a054a75feb78c6d
Amandeep Singh <amandeep.singh@cssindiaonline.com>
2021-03-23 10:43:30 +0530

    implemented autoloading routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 require("dotenv").config();
-
-// import routes
-const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
@@ -24,11 +21,11 @@ mongoose
 
 // middlewares
 app.use(morgan("dev"));
-app.use(bodyParser.json({ limit: "2mb" }));
+app.use(express.json({ limit: "2mb" }));
 app.use(cors());
 
-// middleware routes
-app.use("/api", authRoutes);
+// routes middleware
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
