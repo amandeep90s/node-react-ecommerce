f03b3aca7ab89de6aa022193d79cd40f129a5267
Amandeep Singh <amandeep.singh@cssindiaonline.com>
2021-03-25 11:35:48 +0530

    category crud client requests added

---
+++ client/src/functions/category.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+// Get all categories
+export const getCategories = async () =>
+    await axios.get(`${process.env.REACT_APP_API_URL}/categories`);
+
+// Get a single category
+export const getCategory = async (slug) =>
+    await axios.get(`${process.env.REACT_APP_API_URL}/category/${slug}`);
+
+// Remove a category
+export const removeCategory = async (slug, authtoken) => {
+    return await axios.delete(
+        `${process.env.REACT_APP_API_URL}/category/${slug}`,
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
+};
+
+// Update a category
+export const updateCategory = async (slug, category, authtoken) => {
+    return await axios.put(
+        `${process.env.REACT_APP_API_URL}/category/${slug}`,
+        category,
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
+};
+
+// Create a category
+export const createCategory = async (category, authtoken) => {
+    return await axios.post(
+        `${process.env.REACT_APP_API_URL}/category`,
+        category,
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
+};
