f77704252c33d14a5bdf396d4d08d5fa5f5e8ff6
Amandeep Singh <programmeramandeep@gmail.com>
2021-04-02 11:25:45 +0530

    file resize in react

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const FileUpload = () => {
+    const { user } = useSelector((state) => ({ ...state }));
+
     const fileUploadAndResize = (e) => {
-        //
+        let files = e.target.files;
+
+        if (files) {
+            for (let index = 0; index < files.length; index++) {
+                Resizer.imageFileResizer(
+                    files[index],
+                    720,
+                    720,
+                    "WEBP",
+                    100,
+                    0,
+                    (uri) => {
+                        console.log(uri);
+                    },
+                    "base64"
+                );
+            }
+        }
     };
 
     return (
