f8347f51a24411d6f3be081651cf8e9ebb0ef6ef
Amandeep Singh <programmeramandeep@gmail.com>
2021-04-03 11:19:58 +0530

    default image, edit, delete icons and short descriptions

--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card } from "antd";
+import laptop from "../../images/laptop.png";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
@@ -12,17 +14,22 @@ const AdminProductCard = ({ product }) => {
             cover={
                 <img
                     alt="Product"
-                    src={
-                        images && images.length
-                            ? images[0].url
-                            : "https://via.placeholder.com/150"
-                    }
+                    src={images && images.length ? images[0].url : laptop}
                     style={{ height: "150px", objectFit: "cover" }}
                     className="img-fluid p-2"
                 />
             }
+            actions={[
+                <EditOutlined className="text-warning" />,
+                <DeleteOutlined className="text-danger" />,
+            ]}
         >
-            <Meta title={title} description={description} />
+            <Meta
+                title={title}
+                description={`${
+                    description && description.substring(0, 28)
+                }...`}
+            />
         </Card>
     );
 };
