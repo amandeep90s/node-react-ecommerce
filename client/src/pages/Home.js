47fb7abd602e100dcdea0f63338c7161f0bbf8ba
Amandeep Singh <programmeramandeep@gmail.com>
2021-04-05 18:46:15 +0530

    best sellers added in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
 
 const Home = () => {
     return (
-        <>
+        <div className="pb-5">
             <div className="jumbotron text-danger text-center h1 font-weight-bold">
                 <Jumbotron
                     text={["Latest Products", "New Arrivals", "Best Sellers"]}
@@ -16,7 +17,13 @@ const Home = () => {
             </div>
 
             <NewArrivals />
-        </>
+
+            <div className="text-center p-3 my-5 display-4 jumbotron">
+                Best Sellers
+            </div>
+
+            <BestSellers />
+        </div>
     );
 };
 
