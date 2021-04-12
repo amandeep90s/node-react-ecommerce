c7a9f46c848d121bfb978c0f23eab3153251ac7c
programmeramandeep <programmeramandeep@gmail.com>
2021-04-12 22:37:30 +0530

    filter by categories frontend

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProuctsByFilter, getProductsByCount } from "../functions/product";
+import { getCategories } from "../functions/category";
 import ProductCard from "../components/cards/ProductCard";
-import { Menu, Slider, Spin } from "antd";
-import { DollarOutlined, LoadingOutlined } from "@ant-design/icons";
+import { Checkbox, Menu, Slider, Spin } from "antd";
+import {
+    DollarOutlined,
+    DownSquareOutlined,
+    LoadingOutlined,
+} from "@ant-design/icons";
 
 const { SubMenu, ItemGroup } = Menu;
 
@@ -12,6 +17,8 @@ const Shop = () => {
     const [price, setPrice] = useState([0, 0]);
     const [loading, setLoading] = useState(false);
     const [ok, setOk] = useState(false);
+    const [categories, setCategories] = useState([]);
+    const [categoryIds, setCategoryIds] = useState([]);
 
     let dispatch = useDispatch();
     const { search } = useSelector((state) => ({ ...state }));
@@ -19,6 +26,8 @@ const Shop = () => {
 
     useEffect(() => {
         loadAllProducts();
+        // fetch categories
+        getCategories().then((res) => setCategories(res.data));
     }, []);
 
     // getting products
@@ -42,8 +51,10 @@ const Shop = () => {
     // 2. load products on user search input
     useEffect(() => {
         const delayed = setTimeout(() => {
-            fetchProducts({ query: text });
-        }, 500);
+            if (text.length > 0) {
+                fetchProducts({ query: text });
+            }
+        }, 300);
 
         return () => clearTimeout(delayed);
     }, [text]);
@@ -58,10 +69,51 @@ const Shop = () => {
             type: "SEARCH_QUERY",
             payload: { text: "" },
         });
+        setCategoryIds([]);
         setPrice(value);
         setTimeout(() => {
             setOk(!ok);
-        }, 500);
+        }, 300);
+    };
+
+    // 4. load products based on category
+    const showCategories = () =>
+        categories.map((c) => (
+            <div key={c._id}>
+                <Checkbox
+                    onChange={handleCheck}
+                    className="pb-2 pl-4 pr-4"
+                    value={c._id}
+                    name="category"
+                    checked={categoryIds.includes(c._id)}
+                >
+                    {c.name}
+                </Checkbox>
+                <br />
+            </div>
+        ));
+
+    // handle check for categories
+    const handleCheck = (e) => {
+        dispatch({
+            type: "SEARCH_QUERY",
+            payload: { text: "" },
+        });
+        setPrice([0, 0]);
+        let inTheState = [...categoryIds];
+        let justChecked = e.target.value;
+        let foundInTheState = inTheState.indexOf(justChecked); // index or -1
+
+        // indexOf method ?? if not found returns -1 else return index [1,2,3,4,5]
+        if (foundInTheState === -1) {
+            inTheState.push(justChecked);
+        } else {
+            // if found pull out one item from index
+            inTheState.splice(foundInTheState, 1);
+        }
+
+        setCategoryIds(inTheState);
+        fetchProducts({ category: inTheState });
     };
 
     return (
@@ -71,6 +123,7 @@ const Shop = () => {
                     <h4>Search/Filter</h4>
                     <hr />
                     <Menu defaultOpenKeys={["1", "2"]} mode="inline">
+                        {/* Price */}
                         <SubMenu
                             key="1"
                             title={
@@ -89,6 +142,17 @@ const Shop = () => {
                                     max="4999"
                                 />
                             </div>
+                        </SubMenu>
+                        {/* Category */}
+                        <SubMenu
+                            key="2"
+                            title={
+                                <span className="h6">
+                                    <DownSquareOutlined /> Categories
+                                </span>
+                            }
+                        >
+                            <div>{showCategories()}</div>
                         </SubMenu>
                     </Menu>
                 </div>
