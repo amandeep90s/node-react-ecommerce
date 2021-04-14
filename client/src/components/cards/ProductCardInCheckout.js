d9fab6e0f8cac8dc00f4859d95ed12dbec4fdfd7
programmeramandeep <programmeramandeep@gmail.com>
2021-04-14 21:37:21 +0530

    qunatity update in cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import ModalImage from "react-modal-image";
+import { toast } from "react-toastify";
 import Laptop from "../../images/laptop.png";
 
 const ProductCardInCheckout = ({ product }) => {
@@ -18,6 +19,36 @@ const ProductCardInCheckout = ({ product }) => {
             cart.map((p, index) => {
                 if (product._id === p._id) {
                     cart[index].color = e.target.value;
+                }
+                return cart;
+            });
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+            // update to redux
+            dispatch({
+                type: "ADD_TO_CART",
+                payload: cart,
+            });
+        }
+    };
+
+    const handleQuantityChange = (e) => {
+        let count = e.target.value < 1 ? 1 : e.target.value;
+
+        if (count > product.quantity) {
+            toast.error(`Max available quanity: ${product.quantity}`);
+            return;
+        }
+
+        let cart = [];
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem("cart")) {
+                cart = JSON.parse(localStorage.getItem("cart"));
+            }
+
+            cart.map((p, index) => {
+                if (product._id === p._id) {
+                    cart[index].count = count;
                 }
                 return cart;
             });
@@ -55,6 +86,7 @@ const ProductCardInCheckout = ({ product }) => {
                         name="color"
                         className="form-control"
                         value={product.color}
+                        style={{ height: "auto" }}
                     >
                         {colors.map((c) => (
                             <option key={c} value={c}>
@@ -63,7 +95,17 @@ const ProductCardInCheckout = ({ product }) => {
                         ))}
                     </select>
                 </td>
-                <td>{product.count}</td>
+                <td>
+                    <input
+                        type="number"
+                        value={product.count}
+                        className="form-control"
+                        onChange={handleQuantityChange}
+                        style={{ width: "50px" }}
+                        min={1}
+                        max={100}
+                    />
+                </td>
                 <td>Shipping icon</td>
                 <td>Delete icon</td>
             </tr>
