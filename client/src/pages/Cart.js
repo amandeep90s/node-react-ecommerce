a0895615bca6d7a518d6faee4ee661bfcbb9310c
programmeramandeep <programmeramandeep@gmail.com>
2021-04-14 09:17:13 +0530

    order summary in cart page

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,18 +1,54 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     const { user, cart } = useSelector((state) => ({ ...state }));
 
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-7">
-                    <h4>Cart</h4>
-                    {JSON.stringify(cart)}
-                </div>
+    const getTotal = () => {
+        return cart.reduce((currentValue, nextValue) => {
+            return currentValue + nextValue.count * nextValue.price;
+        }, 0);
+    };
 
-                <div className="col-md-5"></div>
+    return (
+        <div className="container-fluid py-3">
+            <div className="row">
+                <div className="col-md-8">
+                    <h4>Cart / {cart.length} Product(s)</h4>
+                    {!cart.length ? (
+                        <p className="lead">
+                            No products in cart.{" "}
+                            <Link to="/shop">Continue Shopping</Link>{" "}
+                        </p>
+                    ) : (
+                        <h4>er</h4>
+                    )}
+                </div>
+                <div className="col-md-4">
+                    <h4>Order Summary</h4>
+                    <hr />
+                    <p>Products</p>
+                    {cart.map((c, i) => (
+                        <div key={i}>
+                            <p>
+                                {c.title} x {c.count} = ${c.price * c.count}
+                            </p>
+                        </div>
+                    ))}
+                    <hr />
+                    Total : <strong>${getTotal()}</strong>
+                    <hr />
+                    {user ? (
+                        <h4 className="btn btn-sm btn-primary mt-2">
+                            Proceed to Checkout
+                        </h4>
+                    ) : (
+                        <h4 className="btn btn-sm btn-primary mt-2">
+                            Login to Checkout
+                        </h4>
+                    )}
+                </div>
             </div>
         </div>
     );
