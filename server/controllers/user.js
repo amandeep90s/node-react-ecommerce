e296f816cabd8b04a8897bcddffc487c2d4fed24
programmeramandeep <programmeramandeep@gmail.com>
2021-04-15 14:58:22 +0530

    save cart to database

---
+++ server/controllers/user.js
@@ -0,0 +1,48 @@
+const User = require("../models/user");
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.userCart = async (req, res) => {
+    const { cart } = req.body;
+
+    let products = [];
+
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    // check if cart with logged in user id already exist
+    let cartExistByThisUser = await Cart.findOne({
+        orderedBy: user._id,
+    }).exec();
+
+    if (cartExistByThisUser) {
+        cartExistByThisUser.remove();
+    }
+
+    for (let i = 0; i < cart.length; i++) {
+        let object = {};
+
+        object.product = cart[i]._id;
+        object.count = cart[i].count;
+        object.color = cart[i].color;
+        // get price for creating total
+        let { price } = await Product.findById(cart[i]._id)
+            .select("price")
+            .exec();
+        object.price = price;
+
+        products.push(object);
+    }
+
+    let cartTotal = 0;
+    for (let i = 0; i < products.length; i++) {
+        cartTotal = cartTotal + products[i].price * products[i].count;
+    }
+
+    let newCart = await new Cart({
+        products,
+        cartTotal,
+        orderedBy: user._id,
+    }).save();
+
+    res.json({ ok: true });
+};
