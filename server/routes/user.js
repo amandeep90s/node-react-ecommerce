c530bd73e8dff9fe5ed8189880bcf21a0e28ea9d
programmeramandeep <programmeramandeep@gmail.com>
2021-04-18 11:22:38 +0530

    apply discount backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,6 +7,7 @@ const { authCheck } = require("../middlewares/auth");
 
 // controllers
 const {
+    applyCouponToUserCart,
     emptyCart,
     userCart,
     getUserCart,
@@ -17,5 +18,8 @@ router.get("/user/cart", authCheck, getUserCart);
 router.post("/user/cart", authCheck, userCart);
 router.delete("/user/cart", authCheck, emptyCart);
 router.post("/user/address", authCheck, saveAddress);
+
+// coupon
+router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
 
 module.exports = router;
