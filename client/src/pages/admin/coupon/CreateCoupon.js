5e1a1fe7d4082687c4865da4c0c29b694237119d
programmeramandeep <programmeramandeep@gmail.com>
2021-04-18 10:25:44 +0530

    coupon listing and delete

--- client/src/pages/admin/coupon/CreateCoupon.js
+++ client/src/pages/admin/coupon/CreateCoupon.js
@@ -29,11 +29,20 @@ const CreateCoupon = () => {
 
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        loadAllCoupons();
+    }, []);
+
+    const loadAllCoupons = () => {
+        getCoupons().then((res) => setCoupons(res.data));
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setLoading(true);
         createCoupon({ name, expiry, discount }, user.token)
             .then((res) => {
+                loadAllCoupons();
                 setName("");
                 setExpiry("");
                 setDiscount("");
@@ -45,6 +54,25 @@ const CreateCoupon = () => {
                 setLoading(false);
                 toast.error(error.message);
             });
+    };
+
+    const handleRemove = (couponId) => {
+        if (window.confirm("Are you sure?")) {
+            setLoading(true);
+            removeCoupon(couponId, user.token)
+                .then((res) => {
+                    loadAllCoupons();
+                    setLoading(false);
+                    toast.success(
+                        `Coupon ${res.data.name} deleted successfully`
+                    );
+                })
+                .catch((error) => {
+                    console.log(error);
+                    setLoading(false);
+                    toast.error(error.message);
+                });
+        }
     };
 
     return (
@@ -63,7 +91,7 @@ const CreateCoupon = () => {
                             Loading...
                         </h4>
                     ) : (
-                        <h4>Coupons</h4>
+                        <h4>Create Coupon</h4>
                     )}
 
                     <form onSubmit={handleSubmit} className="row">
@@ -128,6 +156,40 @@ const CreateCoupon = () => {
                             </button>
                         </div>
                     </form>
+                    <hr />
+
+                    <h4>{coupons.length} Coupons</h4>
+
+                    <table className="table table-bordered">
+                        <thead className="thead-light">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Expiry</th>
+                                <th scope="col">Discount</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {coupons.map((c, i) => (
+                                <tr key={i}>
+                                    <td>{c.name}</td>
+                                    <td>
+                                        {new Date(
+                                            c.expiry
+                                        ).toLocaleDateString()}
+                                    </td>
+                                    <td>{c.discount}%</td>
+                                    <td>
+                                        <EditOutlined className="text-primary pointer mr-2" />
+                                        <DeleteOutlined
+                                            onClick={() => handleRemove(c._id)}
+                                            className="text-danger pointer"
+                                        />
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
