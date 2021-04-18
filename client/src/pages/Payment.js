6a4633a4d9e30bd7180509426dc73df434112cc6
programmeramandeep <programmeramandeep@gmail.com>
2021-04-18 17:34:33 +0530

    installed stripe packages in client side

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+// load stripe outside of components to avoid recreating object on every render
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const Payment = () => {
     return (
-        <div>
-            <h4>payment</h4>
+        <div className="container text-center p-5">
+            <h4>Complete your purchase</h4>
+            <Elements stripe={promise}>
+                <div className="col-md-8 offset-md-2"></div>
+            </Elements>
         </div>
     );
 };
